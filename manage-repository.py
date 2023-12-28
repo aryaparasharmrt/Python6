04cc5d089916e6d027865d0c8bc7bf65f2267054
Arya Parashar <66545234+aryaparasharmrt@users.noreply.github.com>
2023-12-28 15:43:55 +0530

    Update manage-repository.py

--- manage-repository.py
+++ manage-repository.py
@@ -2,8 +2,6 @@ import subprocess
 
 print('Running repository management tasks...')
 
-# Your repository management tasks go here
-
 # Example: Print the list of branches
 branches = subprocess.check_output(['git', 'branch']).decode('utf-8')
 print('Branches:', branches)
